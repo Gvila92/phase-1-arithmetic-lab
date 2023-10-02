b3f456b84dab90415235a9f6a874f4ca4dd3d1f9
George vila <gvila92@gmail.com>
2023-10-02 16:14:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1= 31;
+let num2 =2;
+let multiply = num1 * num2;
+  console.log(multiply);
 
-// Write your code here
+  let random = Math.floor(Math.random() * 100) + 1;
+   console.log(random);
+
+   let num3 =18;
+   let num4 = 7;
+   let mod = num3 % num4;
+    console.log(mod);
+
+    let set = [10, 5, 20, 15];
+    let max = Math.max(20);
+     console.log(max);
